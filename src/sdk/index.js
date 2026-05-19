d1bd178d833b49e1bc99f51c44498219536926ea
Alexandre Benites <mkt.elvinos@gmail.com>
2026-05-19 00:48:53 -0300

    fix: remove generated .js files, add noEmit to tsconfig

--- src/sdk/index.js
+++
@@ -1,7 +0,0 @@
-export { NewsFeed } from './NewsFeed';
-export { NewsCard } from './NewsCard';
-export { NewsModal } from './NewsModal';
-export { CategoryFilter } from './CategoryFilter';
-export { useNews } from './useNews';
-export * from './types';
-export * from './scheduler';
